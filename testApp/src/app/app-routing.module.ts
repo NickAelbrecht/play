af47c8a3a068ab7fd7ce486dce78d565425f3216
NickAelbrecht <nick.aelbrecht.y9727@student.hogent.be>
2019-05-03 21:33:12 +0200

    Create routing module, navbar/home comp

---
+++ testApp/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent}
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- testApp/src/app/app.module.ts
+++ testApp/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
