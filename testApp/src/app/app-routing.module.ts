2ac7ac40ed3a31470f3d3a69fe0277635acd131e
NickAelbrecht <nick.aelbrecht.y9727@student.hogent.be>
2019-05-05 15:12:46 +0200

    added login/register/logout components

--- testApp/src/app/app-routing.module.ts
+++ testApp/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { RegisterComponent } from './pages/user/register/register.component';
+import { FormComponent } from './pages/form/form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/user/login/login.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'form', component: FormComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 @NgModule({
   declarations: [],
--- testApp/src/app/app.module.ts
+++ testApp/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FormComponent } from './pages/form/form.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { LogoutComponent } from './pages/user/logout/logout.component';
+import { RegisterComponent } from './pages/user/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
